26bb9d8211abd7eb29794bfa159ff53d5ac108f0
collapsedreams <artsint13@gmail.com>
2024-02-20 11:34:23 +0300

    m2h0

---
+++ main2.cpp
@@ -0,0 +1,15 @@
+//
+//  main2.cpp
+//  some
+//
+//  Created by Артём Синцов on 2/20/24.
+//
+
+#include "main2.hpp"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World1!\n";
+    return 0;
+}
